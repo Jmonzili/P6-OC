e93e77435fade3b35ec594914b9338b1a7b910ae
Jeffrey Monzili <jeffrey.monzili@gmail.com>
2022-06-07 19:22:08 +0200

    Ajout du README

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# P6-OC
+# P6 - Piquante
+
+Développeur Web - Construisez une API sécurisée avec JavaScript Auteur
+
+👤 Jeffrey MONZILI
+
+Développement Backend en JavaScript
+
+Serveur Node.js
+Framework Express
+Base de données MongoDB
+    Hébergement sur MongoDB Atlas
+    Opérations relatives à la BDD réalisées avec mongoose
+API REST
+Sécurité OWASP et RGPD
+
+
+Faire fonctionner l'application Cloner le Frontend
+
+Cloner le frontend de l'application.
+Installer les dépendances : npm install puis npm install --save-dev run-script-os.
+Lancer : npm start
+
+
+Cloner le Backend
+
+Cloner le backend de l'application.
+Pour le projet, le fichier .env est ignoré dans gitignore
+installer les dépendances : npm install
+lancer : nodemon server
+
+
+Template du fichier .env
+
+DB_USER = "******"
+DB_PASSWORD = "******"
+DB_HOST = "******"
+DB_NAME = "******"
+
+JWT_PASSWORD = "******"
--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -7,16 +7,19 @@ async function createUser(req, res) {
   try { 
     const { email, password } = req.body
     const hashedPassword = await hashPassword(password)
+// Récupération de l'email et du password dans la requete
     const user = new User({ email, password: hashedPassword});
+// Sauvegarde du user dans la database
     await user.save()
-    res.status(201).send({ message: 'Utilisateur bien été reçue !' })
-  } 
+    res.status(201).send({ message: "Utilisateur enregistré !" })
+  }
+// En cas d'erreur renvoi d'un code 409
   catch(err) {
     res.status(409).send({ message: "User non enregistré:" + err })
   }
 }
 
-// Cryptage du MDP
+// Cryptage du MDP via bcrypt
 function hashPassword(password) {
   const saltRounds = 10;
   return bcrypt.hash(password, saltRounds)
@@ -29,23 +32,28 @@ async function logUser(req, res) {
     const password = req.body.password
     const user = await User.findOne( {email: email})
   
-  // Vérification MDP
+// Vérification MDP
     const passwortCtrl = await bcrypt.compare(password, user.password)
+// Condition en cas de mot de passe incorrect
     if (!passwortCtrl) {
       return res.status(403).send({ error: 'Mot de passe incorrect !' });
     }
+// Création du token via la fonction 
     const token = createToken(email)
     res.status(200).send({ userId: user._id, token: token})
-  } 
+  }
+// Renvoi de l'erreur et d'un code 500
   catch(err) {
     console.error(err)
     res.status(500).send({ message: "Erreur de connexion" })
   }
 }
 
-// Création TOKEN
+// Fonction de création TOKEN
 function createToken(email) {
+// Récupération du password token dans le fichier env
   const jwtPassword = process.env.JWT_PASSWORD
+//
   return jwt.sign({email: email}, jwtPassword, { expiresIn: "24h" })
 }
 
