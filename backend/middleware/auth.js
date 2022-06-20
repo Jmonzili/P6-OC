204b97039eff1e02b2a167f89f71a1097a4ed814
Jeffrey Monzili <jeffrey.monzili@gmail.com>
2022-06-20 10:23:38 +0200

    Correction des authentification + commentaire

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -4,13 +4,16 @@ const jwt = require("jsonwebtoken")
 function authenticateUser(req, res, next) {
   try {
     const token = req.headers.authorization.split(' ')[1];
+  // Décodage du token via la clef secret
     const decodedToken = jwt.verify(token, `${process.env.JWT_PASSWORD}`);
     const userIdDecodedToken = decodedToken.userId;
 
-    // Vérification de la concordance entre les clés utilisateurs
-    if (req.body.userId && req.body.userId !== userIdDecodedToken) {
+// Vérification de la concordance entre le userId de la requete et du userId décodé
+// Bloqué si ils sont différent
+    if (req.body.userId && (req.body.userId !== userIdDecodedToken)) {
       throw 'User ID non valable !';
     }
+// Poursuivre si ils sont identique
     next();
   } catch (error) {
     res.status(401).json({ error: error | 'Requête non authentifiée !' });
